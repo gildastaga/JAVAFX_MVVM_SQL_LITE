9345db8fe460f147bdc024a2b5cb7accfdd231c5
Soh momo marceline <marcelinealvinemomosoh@yahoo.fr>
2021-01-19 19:54:47 +0100

    test

---
+++ anc3_2021_projet_C05/src/main/java/view/View.java
@@ -0,0 +1,15 @@
+ package view;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class View extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
